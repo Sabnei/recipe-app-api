558ab5d32d07a5bb1524f2df7028cf759be98665
Sabnei <sebasplay417@gmail.com>
2025-01-14 17:28:11 -0600

    Mejora la personalización del administrador de Django para el modelo de usuario y corrige el método de configuración de pruebas.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -9,7 +9,7 @@ from django.urls import reverse
 class AdminSiteTests(TestCase):
     """Tests for Django admin modifications"""
 
-    def setUP(self):
+    def setUp(self):
         """Create user and client."""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
@@ -30,3 +30,10 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page view."""
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
